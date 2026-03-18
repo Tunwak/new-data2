c852e7e8562c953011b67c36772a55731744aa76
Tunwa Kheanphet <tunwa.k@psru.ac.th>
2026-03-18 16:35:19 +0700

    new

--- server.js
+++ server.js
@@ -109,6 +109,6 @@ app.get("/", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-  console.log("🔥 Server Running");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
